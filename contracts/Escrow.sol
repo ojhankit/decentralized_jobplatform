866b4a64cac34ad09f9af3944c52e4d6e14fa34b
ojhankit <ojha27ankit@gmail.com>
2025-10-22 08:30:33 +0000

    escrow contract all methods

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -28,6 +28,49 @@ contract Escrow {
     function deposit(uint job_id) external payable {
         require(msg.value > 0, "Deposit must be greater than 0");
         require(job_funds[job_id] == 0, "already deposited");
-        
+        require(!refunded[job_id] && !released[job_id], "Job already processed");
+
+        job_funds[job_id] = msg.value;
+        job_client[job_id] = msg.sender;
+
+        emit Deposited(job_id, msg.sender, msg.value);
+    }
+
+    // called by jobmanager when job is closed / completed
+    function releasePayment(uint job_id, address payable freelancer) external onlyJobManager{
+        require(!released[job_id], "Already released");
+         require(!refunded[job_id], "Already refunded");
+        require(job_funds[job_id] > 0, "No funds for this job");
+
+        uint amount = job_funds[job_id];
+        released[job_id] = true;
+        job_funds[job_id] = 0;
+
+        (bool success, ) = freelancer.call{value: amount}("");
+        require(success, "Transfer failed");
+
+        emit Released(job_id, freelancer, amount);
+    }
+
+    // refund client if job is cancelled
+    function refundClient(uint job_id) external onlyJobManager {
+        require(!refunded[job_id], "Already refunded");
+        require(!released[job_id], "Already released");
+        require(job_funds[job_id] > 0, "No funds for this job");
+
+        uint amount = job_funds[job_id];
+        refunded[job_id] = true;
+        job_funds[job_id] = 0;
+
+        address payable client = payable(job_client[job_id]);
+        (bool success, ) = client.call{value: amount}("");
+        require(success, "Refund failed");
+
+        emit Refunded(job_id, client, amount);
+    }
+
+    // optional method to check escrow balance
+    function getEscrowBalance(uint job_id) external view returns (uint) {
+        return job_funds[job_id];
     }
 }
