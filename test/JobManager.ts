24a9a220bb29c2350059c3708719d62b7ef2499c
ojhankit <ojha27ankit@gmail.com>
2025-10-24 07:15:24 +0000

    job manager test

--- test/JobManager.ts
+++ test/JobManager.ts
@@ -1,26 +1,31 @@
 import { expect } from "chai";
+import { Contract, Signer } from "ethers";
 import { network } from "hardhat";
 
 const { ethers } = await network.connect();
 
+/* 
+1. setup & deployment
+2. job creation
+3. assigning job
+4. deposit flow
+5. job completion
+6. job close
+7. job cancel
+8. access ctrl
+*/
 
 describe("JobManager", function () {
-  it("Should emit JobPosted when a job is posted", async function () {
-    const jobManager = await ethers.deployContract("JobManager");
-    const [owner, client] = await ethers.getSigners();
+  let userManager: Contract;
+  let jobManager: Contract;
+  let escrow: Contract;
 
-    await expect(
-      jobManager.connect(client).postJob("Build a website", "ipfs://jobDesc123", 1000)
-    )
-      .to.emit(jobManager, "JobPosted")
-      .withArgs(1, client.address, "Build a website", 1000);
+  let employer: Signer;
+  let freelancer: Signer;
 
-    const job = await jobManager.getJob(1);
-    expect(job.id).to.equal(1);
-    expect(job.client).to.equal(client.address);
-    expect(job.title).to.equal("Build a website");
-    expect(job.description_url).to.equal("ipfs://jobDesc123");
-    expect(job.budget).to.equal(1000);
-  });
+  beforeEach(async function () {
+    [ employer, freelancer ] = await ethers.getSigners();
 
+    // deploy mock user manager
+  })
 });
